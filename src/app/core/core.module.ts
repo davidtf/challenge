6d47820a669e56717d4fc11423481e81318b7254
David Finley <512650+davidtf@users.noreply.github.com>
2019-08-21 11:56:39 -0400

    Add Navigation (header and sidenav)
    This provides a top-level material toolbar and clickable sidenav.
    A dummied username dropdown menu is also available.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavigationComponent } from './navigation/navigation.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavigationComponent],
   imports: [
-    CommonModule
+    BrowserAnimationsModule,
+    CommonModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    RouterModule
+  ],
+  exports: [
+    NavigationComponent
   ]
 })
 export class CoreModule { }
---
+++ src/app/core/navigation/navigation.component.ts
@@ -0,0 +1,29 @@
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnDestroy {
+
+  mobileQuery: MediaQueryList;
+
+  private mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.mobileQueryListener = () => changeDetectorRef.detectChanges();
+
+    this.mobileQuery.addEventListener('change', () => {
+      this.mobileQueryListener();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeEventListener('change', () => {
+      this.mobileQueryListener();
+    });
+  }
+}
